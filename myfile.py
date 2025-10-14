b905478c80fd39a9db4dccd22c5066b8b9191b17
Mike <mike_ghazi@hotmail.com>
2025-10-13 23:43:03 -0500

    add myfile.py

---
+++ myfile.py
@@ -0,0 +1,9 @@
+"print('hello, github')" 
+
+def greet(name):
+    """Greet someone by name"""
+    return f"hello, {name} Welcome to Github"
+
+if __name__ == "__main__":
+    print(greet("world"))
+    print("This is my first github test")
